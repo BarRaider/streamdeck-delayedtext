c51bb383286affb286142fcbf1a5e1e5e42b102c
BarRaider <nospam+gh@blachshma.com>
2019-01-12 18:41:58 -0800

    Initial commit

---
+++ DelayedText/DelayedTextInput.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+
+namespace Delayedtext
+{
+    public class DelayedTextInput
+    {
+        private class InspectorSettings
+        {
+            public static InspectorSettings CreateDefaultSettings()
+            {
+                InspectorSettings instance = new InspectorSettings();
+                instance.InputText = String.Empty; ;
+                instance.Delay = 1;
+
+                return instance;
+            }
+
+            public async void SendToPropertyInspectorAsync()
+            {
+                if (StreamDeckConnection != null && !String.IsNullOrEmpty(ContextId) && !String.IsNullOrEmpty(ActionId))
+                {
+                    await StreamDeckConnection.SendToPropertyInspectorAsync(ActionId, JObject.FromObject(this), this.ContextId);
+                }
+            }
+
+            public async void SetSettingsAsync()
+            {
+                if (StreamDeckConnection != null && !String.IsNullOrEmpty(ContextId) && !String.IsNullOrEmpty(ActionId))
+                {
+                    await StreamDeckConnection.SetSettingsAsync(JObject.FromObject(this), this.ContextId);
+                }
+            }
+
+            [JsonProperty(PropertyName = "inputText")]
+            public string InputText { get; set; }
+
+            [JsonProperty(PropertyName = "delay")]
+            public int Delay { get; set; }
+
+            [JsonIgnore]
+            public string ActionId { private get; set; }
+
+            [JsonIgnore]
+            public string ContextId { private get; set; }
+
+            [JsonIgnore]
+            public streamdeck_client_csharp.StreamDeckConnection StreamDeckConnection { private get; set; }
+        }
+
+        #region Private members
+
+        private const int RESET_COUNTER_KEYPRESS_LENGTH = 1;
+
+        private bool inputRunning = false;
+        private InspectorSettings settings;
+
+        #endregion
+
+        #region Public Methods
+
+        public DelayedTextInput(streamdeck_client_csharp.StreamDeckConnection connection, string action, string context, JObject settings)
+        {
+            if (settings == null || settings.Count == 0)
+            {
+                this.settings = InspectorSettings.CreateDefaultSettings();
+            }
+            else
+            {
+                this.settings = settings.ToObject<InspectorSettings>();
+            }
+
+            this.settings.StreamDeckConnection = connection;
+            this.settings.ActionId = action;
+            this.settings.ContextId = context;
+        }
+
+        public void KeyPressed()
+        {
+            if (inputRunning)
+            {
+                return;
+            }
+            
+            SendInput();
+        }
+
+        private async void SendInput()
+        {
+            inputRunning = true;
+            await Task.Run(() =>
+            {
+                InputSimulator iis = new InputSimulator();
+                string text = settings.InputText;
+                int delay   = settings.Delay;
+
+                for (int idx = 0; idx < text.Length; idx++)
+                {
+                    iis.Keyboard.TextEntry(text[idx]);
+                    Thread.Sleep(delay);
+                }
+            });
+            inputRunning = false;
+        }
+
+        public void KeyReleased()
+        {
+        }
+
+        public void UpdateSettings(JObject payload)
+        {
+            if (payload["property_inspector"] != null)
+            {
+                switch (payload["property_inspector"].ToString().ToLower())
+                {
+                    case "propertyinspectorconnected":
+                        settings.SendToPropertyInspectorAsync();
+                        break;
+
+                    case "propertyinspectorwilldisappear":
+                        settings.SetSettingsAsync();
+                        break;
+
+                    case "updatesettings":
+                        settings.Delay = (int)payload["delay"];
+                        settings.InputText = (string)payload["inputText"];
+                        settings.SetSettingsAsync();
+                        break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # streamdeck-delayedtext
+A C# delayed text inputer for the Elgato Stream Deck device.
+
+## Dependencies
+This library uses the [streamdeck-client-csharp](https://github.com/TyrenDe/streamdeck-client-csharp) wrapper
+
+## Current functionality
+- Simulates keyboard input
+- You can choose the delay between each character (between 1ms to 2000ms)
+
+## Feature roadmap
+Always open to more suggestions.
+
+## How do I get started using it?
+Install by clicking the com.barraider.delayedtext.streamDeckPlugin file in the Releases folder.
+
+## I found a bug, who do I contact?
+Just head over to the issues page and create a new issue.
+
+## License
+MIT License
+
+Copyright (c) 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
